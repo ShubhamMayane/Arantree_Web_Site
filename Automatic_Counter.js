ed0c98abdcd269c8254a18f5cf95c0958f6707e4
ShubhamMayane <shubhammayane@gmail.com>
2025-03-07 02:08:47 +0530

    Initial commit of my static website

---
+++ Automatic_Counter.js
@@ -0,0 +1,66 @@
+
+        //DOM element for the automatic counter logic
+        const counterContainer=document.getElementById("services-section-2-container");
+        const counterDiv1 = document.getElementById("count-1")
+        const counterDiv2 = document.getElementById("count-2")
+    
+        //DOM element for the form submit logic 
+        // variables definition section 
+        /*variables for automatic counter logic */
+        let counter1 = 0;
+        let counter2 = 0;
+        let interval1;
+        let interval2;
+        const targetNumber1 = 70; // Set the target number you want to count up to
+        const targetNumber2 = 26;// Set the target number you want to count up to
+    
+        
+            // Function to start counting
+        function startCounter() {
+     
+       
+            // Reset counter if it's already running
+            clearInterval(interval1);
+            clearInterval(interval2);
+    
+            //following 2 setInterval methods gets called asynchronously i.e ekach veli
+            //for counter 1
+    
+            interval1 = setInterval(function() {
+                counter1++;
+                counterDiv1.innerHTML = "+"+counter1+"%";
+        
+                // Stop the counter when it reaches the target number
+                if (counter1 >= targetNumber1) {
+                    clearInterval(interval1);
+                }
+            }, 10); // Increment every 50ms for smooth counting
+    
+    
+            //for counter 2
+            interval2=setInterval(function() {
+                counter2++;
+                counterDiv2.innerHTML = counter2+"%";
+        
+                // Stop the counter when it reaches the target number
+                if (counter2 >= targetNumber2) {
+                    clearInterval(interval2);
+                }
+            }, 10); 
+        }
+    
+          // IntersectionObserver to detect when the div is in view
+            const observer = new IntersectionObserver(entries => {
+            console.log("div is in focus");
+                            
+                            entries.forEach(entry => {
+                                if (entry.isIntersecting) {
+                                    startCounter(); // Start the counter when div comes into view
+                                    observer.disconnect(); // Stop observing once the counter has started
+                                }
+                            });
+                        }, {
+                            threshold: 0.5 // Start counting when 50% of the div is visible in the viewport
+                        });
+    
+            observer.observe(counterContainer); // Start observing the counterContainer div
---
+++ My_Study/Git_Hub_Use/How_To_Deploy_Static_website_On_Github.md
@@ -0,0 +1,32 @@
+
+
+Steps to deploy youy static website on the github:
+
+Note:tuzhya static web site chya project madhe entry poiint html i.e home page chya html che nav
+index.html ch asave ***this the the rule from the github.
+
+
+
+
+Step 1: Enable GitHub Pages
+Go to your repository on GitHub.
+
+2.Click on the Settings tab (on the right side).
+
+3.click the "Pages" section (under the Code and automation section).
+
+Under Source, choose the branch to deploy from: i.e (in your case) select main branch 
+
+If you're using the master branch, select master (or main if that's your default branch).
+Optionally, you can select the /root folder or /docs if your files are in a specific folder (but typically, the root works for a simple website).
+Click Save.
+
+Step 5: Access Your Website
+
+After a few moments, GitHub will deploy your website. and 
+will give you the link of website on the same page just refresh this page after few seconds
+
+You can now access your website at:
+arduino
+Copy
+https://your-username.github.io/your-repository-name/
